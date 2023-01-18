d81f8b632fc368a4fa358c5314da204ded4052c6
pwl-ev <pwl93.e@gmail.com>
2023-01-19 01:14:01 +0300

    add/delete function for basket

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,13 @@ from app import app, session
 from flask import render_template, redirect, flash, request, url_for
 from app.forms import LoginForm, RegistrationForm
 from flask_login import login_user, current_user, login_required, logout_user
-from app.models import User, Part, ElementImage
+from app.models import User, Part, ElementImage, Order, Basket
 from werkzeug.urls import url_parse
+from sqlalchemy import func
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
     brands = session.query(Part).group_by(Part.brand_id)
     return render_template('index.html', title='WLCM T CMPS', brands=brands)
@@ -102,6 +103,62 @@ def part(brand_name, brand_id, type_id, model_name, model_id, element_name, elem
     return render_template('part.html', parts=parts, image=image)
 
 
-# админка
-# @app.route('/admin')
+@app.route('/basket')
+def basket():
+    order = session.query(Order).filter(Order.user_id == current_user.id).filter(Order.status == 0).first()
+    # добавить проверку есть ли уже созданный заказ или нет, если нет то создать
+    basket = session.query(Basket).filter(Basket.order_id == order.id).group_by(Basket.part_id).all()
+    return render_template('basket.html', basket=basket)
+
+
+@app.route('/addtobasket/<part_id>')
+@login_required
+def addToBasket(part_id):
+
+    order = session.query(Order).filter_by(user_id=current_user.id).filter(Order.status == 0).first()
+    part = session.query(Part).get(part_id)
+    if order == None:
+        order = Order(user=current_user)
+        session.add(order)
+        session.commit()
+        basket = Basket(amount=1, part=part, order=order)
+        session.add(basket)
+        session.commit()
+    else:
+        basket = session.query(Basket).filter_by(order_id=order.id).filter_by(part_id=part.id).first()
+
+        if basket == None:
+            basket = Basket(amount=1, part=part, order=order)
+            session.add(basket)
+            session.commit()
+        else:
+            basket.amount = basket.amount + 1
+            session.commit()
+
+        flash(part.description + ' добавлен(-а) в корзину!')
+
+    return redirect(url_for('part', brand_name=part.brand.brand_name, brand_id=part.brand_id, type_id=part.type_id,
+                     model_name=part.model.model_name, model_id=part.model_id, element_name=part.element.element_name,
+                     element_id=part.element_id))
+
+
+@app.route('/deletefrombasket/<order_id>/<part_id>')
+def deleteFromBasket(order_id, part_id):
+
+    part = session.query(Basket).filter_by(order_id=order_id).filter_by(part_id=part_id).first()
+    # if part == None:
+    #     description = 'Корзина пуста'
+
+    if part.amount > 1:
+        part.amount = part.amount - 1
+        session.commit()
+        description = part.part.description
+    else:
+        description = part.part.description
+        session.query(Basket).filter_by(order_id=order_id).filter_by(part_id=part_id).delete()
+        session.commit()
+
+    flash(description + ' удален(-а)!')
+    return redirect(url_for('basket'))
+
 
